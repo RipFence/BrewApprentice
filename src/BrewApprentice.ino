ffb54599c0d1727254765187416348bfa486b142
Tim Kremer <tim@Tims-Mac.foolproof.net>
2018-08-11 15:40:11 -0500

    Added DS18B20 and ITEAD_Particle_Nextion libraries

---
+++ src/BrewApprentice.ino
@@ -0,0 +1,20 @@
+/*
+ * Project BrewApprentice
+ * Description:
+ * Author:
+ * Date:
+ */
+#include "ITEADLIB_Particle_Nextion.h"
+#include "DS18B20.h"
+
+// setup() runs once, when the device is first turned on.
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
